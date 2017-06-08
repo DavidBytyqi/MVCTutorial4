8e2141ce617950f8fa0d2623cee728b2407c328e
davidb <davidb@APPDEC-PC>
2017-06-08 11:06:54 +0200

    Add project files.

---
+++ MVCTutorial4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCTutorial4.Startup))]
+namespace MVCTutorial4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
